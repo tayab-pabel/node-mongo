c869d46ccd4c28dc98ea1945560d48ae48defc6e
tayab-pabel <tayabpabel@yahoo.com>
2020-09-27 22:39:39 +0600

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('I know how to open node')
-})
+app.use(cors());
+app.use(bodyParser.json());
 
-app.listen(4200, () => console.log('Listening to port 3000'))
+const users = ['Tayab', 'Pabel', 'Abu', 'Md', 'Hridou', 'Yeasfi'];
+
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name});
+});
+
+// post 
+app.post('/addUser', (req, res) => {
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
+});
+
+app.listen(4200, () => console.log('Listening to port 4200'));
